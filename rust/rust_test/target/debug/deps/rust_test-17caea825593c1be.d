cf31eacce90868aa6e9e5bae6aa27d32149b6e35
bradley quinn <bradleyquinn@bradleys-Laptop.attlocal.net>
2025-01-17 23:00:59 -0600

    simple cargo build test

---
+++ rust/rust_test/target/debug/deps/rust_test-17caea825593c1be.d
@@ -0,0 +1,5 @@
+/Users/bradleyquinn/Projects/Vizsla/rust_test/target/debug/deps/rust_test-17caea825593c1be: src/main.rs
+
+/Users/bradleyquinn/Projects/Vizsla/rust_test/target/debug/deps/rust_test-17caea825593c1be.d: src/main.rs
+
+src/main.rs:
