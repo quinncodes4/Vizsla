ced08a711f39889eccd1251d15bcf068ade141ae
bradley quinn <bradleyquinn@bradleys-Laptop.attlocal.net>
2025-01-20 16:19:26 -0600

    learning how to split strings

--- rust/examples/functions/src/main.rs
+++ rust/examples/functions/src/main.rs
@@ -2,15 +2,25 @@ fn main() {
     let x: u32 = 5;
     let y: u32 = 5;
 
+    let st = "this land was made for you and me";
     print_shit(x);
 
     println!("x*y is: {}",mult(x,y));
+    println!("split string is: {}",stringIndexValue(st.to_string(),6));
 }
 
+//print method but must take u32
 fn print_shit(x: u32){
     println!("x is: {}",x);
 }
 
 fn mult(x: u32,y: u32) -> u32 {
     return &x * &y;
+}
+
+
+//function to print value of string at index, i.e its char
+fn stringIndexValue(a: String,i:usize) -> String {
+    let s = a.chars().nth(i).unwrap();
+    return String::from(s);
 }
