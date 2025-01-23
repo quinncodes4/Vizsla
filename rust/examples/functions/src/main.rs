b8cdd518078b28f521e0412c529fecb7fd7a3069
bradley quinn <bradleyquinn@bradleys-Laptop.attlocal.net>
2025-01-23 16:13:20 -0600

    learning mod(modules?)

--- rust/examples/functions/src/main.rs
+++ rust/examples/functions/src/main.rs
@@ -1,3 +1,6 @@
+
+mod test;
+
 fn main() {
     let x: u32 = 5;
     let y: u32 = 5;
@@ -8,19 +11,18 @@ fn main() {
     //println!("x*y is: {}",mult(x,y));
     println!("split string is: {}",stringIndexValue(st.to_string(),6));
     loopString(st.to_string());
+    test::function();
 }
 
 //print method but must take u32
 fn print_shit(x: u32){
     println!("x is: {}",x);
 }
-
 fn mult(x: u32,y: u32) -> u32 {
     return &x * &y;
 }
 
-
-//function to print value of string at index, i.e its char
+//function to print value of string at index, i.e it chars
 fn stringIndexValue(a: String,i:usize) -> String {
     let s = a.chars().nth(i).unwrap();
     return String::from(s);
---
+++ rust/examples/functions/src/test.rs
@@ -0,0 +1,5 @@
+
+//by default all functions in rust are privtate(i think?), use pub keyword to declare its public
+pub fn function(){
+	println!("This a is function call from test.rs");
+}
